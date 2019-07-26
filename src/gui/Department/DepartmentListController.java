701227f8eba1a2f0cec8697774483ce51796e016
Abel Cabral <jcabral@id.uff.br>
2019-07-26 06:22:24 -0300

    Update department

--- src/gui/Department/DepartmentListController.java
+++ src/gui/Department/DepartmentListController.java
@@ -4,6 +4,7 @@ import application.Main;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,10 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
@@ -41,6 +39,9 @@ public class DepartmentListController implements Initializable, DataChangeListen
     private TableColumn<Department, String> tableColumnName;
 
     @FXML
+    private TableColumn<Department, Department> tableColumnEdit;
+
+    @FXML
     private Button button;
 
     private ObservableList<Department> obsList;
@@ -53,7 +54,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       initializeNodes();
+        initializeNodes();
     }
 
     // Ao inves de injetar na inicializacao, fazemos isso numa funcao
@@ -85,6 +86,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 
         obsList = FXCollections.observableArrayList(list);
         tableViewDepartment.setItems(obsList);
+        initEditButtons();      // add um botao em cada linha da tabela
     }
 
     private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
@@ -122,5 +124,24 @@ public class DepartmentListController implements Initializable, DataChangeListen
     @Override
     public void onDataChanged() {           //Quem está escutando e irá executar uma ação
         updateTableView();      // Recarrega a lista
+    }
+
+    private void initEditButtons() {
+        tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+        tableColumnEdit.setCellFactory(param -> new TableCell<>() {
+            private final Button button = new Button("edit");
+
+            @Override
+            protected void updateItem(Department obj, boolean empty) {
+                super.updateItem(obj, empty);
+                if (obj == null) {
+                    setGraphic(null);
+                    return;
+                }
+                setGraphic(button);
+                button.setOnAction(
+                        event -> createDialogForm(obj, "/gui/Department/DepartmentForm.fxml", Utils.currentStage(event)));
+            }
+        });
     }
 }
